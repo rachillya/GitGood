347eb2d35517750df15e6de26bdf6838f2fca6c7
rachillya <95993907+rachillya@users.noreply.github.com>
2021-12-12 00:19:13 -0700

    Add files via upload

---
+++ server/src/main/java/com/tco/MicroServer.java
@@ -0,0 +1,113 @@
+package com.tco.server;
+
+import com.tco.misc.BadRequestException;
+import com.tco.misc.JSONValidator;
+import com.tco.requests.ConfigRequest;
+import com.tco.requests.FindRequest;
+import com.tco.requests.DistancesRequest;
+import com.tco.requests.ShorterTrip.TourRequest;
+import com.tco.requests.Request;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
+import static spark.Spark.*;
+
+class MicroServer {
+
+    private final Logger log = LoggerFactory.getLogger(MicroServer.class);
+    private DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
+    private final int HTTP_OK = 200;
+    private final int HTTP_BAD_REQUEST = 400;
+    private final int HTTP_SERVER_ERROR = 500;
+
+    MicroServer(int serverPort) {
+        configureServer(serverPort);
+        processRestfulAPIrequests();
+    }
+
+    /* Configure MicroServices Here. */
+
+    private void processRestfulAPIrequests() {
+        path("/api", () -> {
+            before("/*", (req, res) -> logRequest(req));
+            post("/config", (req, res) -> processHttpRequest(req, res, ConfigRequest.class));
+            post("/find", (req, res) -> processHttpRequest(req, res, FindRequest.class));
+            post("/distances", (req, res) -> processHttpRequest(req, res, DistancesRequest.class));
+            post("/tour", (req, res) -> processHttpRequest(req, res, TourRequest.class));
+        });
+    }
+
+    /* You shouldn't need to change what is found below. */
+
+    private String processHttpRequest(spark.Request httpRequest, spark.Response httpResponse, Type requestType) throws BadRequestException {
+        setupResponse(httpResponse);
+        String jsonString = httpRequest.body();
+        try {
+            if(jsonString.contains("type") || jsonString.contains("where")){
+                log.info("Bad Request - Find request contained parameter for 'type' or 'where");
+                httpResponse.status(HTTP_BAD_REQUEST);
+                throw new BadRequestException();
+            }
+            JSONValidator.validate(jsonString, requestType);
+            Request requestObj = new Gson().fromJson(jsonString, requestType);
+            return buildJSONResponse(requestObj);
+        } catch (IOException | BadRequestException e) {
+            log.info("Bad Request - {}", e.getMessage());
+            httpResponse.status(HTTP_BAD_REQUEST);
+        } catch (Exception e) {
+            log.info("Server Error - ", e);
+            httpResponse.status(HTTP_SERVER_ERROR);
+        }
+        return jsonString;
+    }
+
+    private void setupResponse(spark.Response response) {
+        response.type("application/json");
+        response.header("Access-Control-Allow-Origin", "*");
+        response.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+        response.status(HTTP_OK);
+    }
+
+    private String buildJSONResponse(Request request) throws BadRequestException {
+        request.buildResponse();
+        String responseBody = new Gson().toJson(request);
+        log.trace("Response - {}", responseBody);
+        return responseBody;
+    }
+
+    private void logRequest(spark.Request request) {
+        String message = "Request - "
+                + "[" + dateTimeFormat.format(LocalDateTime.now()) + "] "
+                + request.ip() + " "
+                + "\"" + request.requestMethod() + " "
+                + request.pathInfo() + " "
+                + request.protocol() + "\" "
+                + "[" + request.body() + "]";
+        log.info(message);
+    }
+
+    private void configureServer(int serverPort) {
+        port(serverPort);
+        String keystoreFile = System.getenv("KEYSTORE_FILE");
+        String keystorePassword = System.getenv("KEYSTORE_PASSWORD");
+        if (keystoreFile != null && keystorePassword != null) {
+            secure(keystoreFile, keystorePassword, null, null);
+            log.info("MicroServer running using HTTPS on port {}.", serverPort);
+        } else {
+            log.info("MicroServer running using HTTP on port {}.", serverPort);
+        }
+
+        // To Serve Static Files (SPA)
+
+        staticFiles.location("/public/");
+        redirect.get("/", "/index.html");
+    }
+}
